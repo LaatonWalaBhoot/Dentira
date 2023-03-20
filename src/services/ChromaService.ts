d9fed625046e704156a90ff2f082bde0a5293d32
laatonwalabhoot <aishwarya.mailin@gmail.com>
2023-03-21 00:04:22 +0530

    added croma service

--- src/services/ChromaService.ts
+++ src/services/ChromaService.ts
@@ -1,14 +1,20 @@
 import {Product, StoreList} from "../data/model/response/CommonResponse";
 import axios from "axios";
-import {load} from "cheerio";
-import * as xml2js from 'xml2js';
-import {chromaService} from "./index";
 
 export default class ChromaService {
 
     async getResults(query: String) {
-        return <StoreList> {
+        const response = await axios.get(`https://api.croma.com/product/allchannels/v1/search?currentPage=0&query=${query}`);
+        const results: Product[] = [];
 
-        }
+        response.data.products.forEach((product: any) => {
+            const title = product.name
+            const image = product.plpImage
+            const price = product.price.value
+
+            results.push(<Product> { name: title, image: image, price: price })
+        })
+
+        return <StoreList> {"croma": results}
     }
 }
