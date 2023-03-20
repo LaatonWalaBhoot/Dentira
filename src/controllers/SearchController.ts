829770d49de91286f6bc5cb82299bd9d2d2fd967
laatonwalabhoot <aishwarya.mailin@gmail.com>
2023-03-20 21:02:30 +0530

    setup controllers

---
+++ src/controllers/SearchController.ts
@@ -0,0 +1,12 @@
+import BaseController from "./BaseController";
+
+export default class SearchController extends BaseController {
+
+    public async getSearchResults(query: String) {
+        return query
+    }
+
+    private formatSearchResponse() {
+
+    }
+}
