fb4645afc800bb1cb429cb88eb4b3d124479ee09
laatonwalabhoot <aishwarya.mailin@gmail.com>
2023-03-20 22:10:38 +0530

    added service response structure

--- src/services/FlipkartService.ts
+++ src/services/FlipkartService.ts
@@ -1,6 +1,10 @@
+import {StoreList} from "../data/model/response/CommonResponse";
+
 export default class FlipkartService {
 
     async getResults(query: String) {
+        return <StoreList> {
 
+        }
     }
 }
--- src/services/RelianceService.ts
+++ src/services/RelianceService.ts
@@ -1,6 +1,10 @@
+import {StoreList} from "../data/model/response/CommonResponse";
+
 export default class RelianceService {
 
     async getResults(query: String) {
+        return <StoreList> {
 
+        }
     }
 }
